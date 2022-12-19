c99dd3f6808462abb994c31252fb9086e25e5d3e
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-19 23:24:26 +0530

    updating categories

--- api/category.ts
+++ api/category.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import { Post, EntityId } from "../shared/types";
 import { config } from "./config";
 
-export async function fetchPosts(categoryId: EntityId): Promise<Post[]> {
+export async function fetchCategory(categoryId: EntityId): Promise<Post[]> {
   const url = `${config.baseUrl}/categories/${categoryId}`;
   const res = await fetch(url);
   return (await res.json()) as Promise<Post[]>;
