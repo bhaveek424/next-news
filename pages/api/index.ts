c7e5f487096d023d4d716ce50c7aafaaa76883b1
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-12-19 22:49:42 +0530

    posts api

---
+++ pages/api/index.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function hello(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ hello: "World" });
+}
